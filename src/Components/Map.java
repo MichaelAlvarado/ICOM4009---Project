045ed8444e6d46bd9d2b5121d8d3322f4cffad22
Jorge Calderon <jorge.calderon3@upr.edu>
2020-02-29 17:09:51 -0400

    Partially Implemented Map class

--- src/Components/Map.java
+++ src/Components/Map.java
@@ -1,5 +1,70 @@
 package Components;
 
-public class Map {
+import java.util.LinkedList;
 
+public class Map {
+	
+	private LinkedList<Building> buildingList;
+	private String buildingName;
+	private int height;
+	private int width;
+	//piso no se que va aqui
+	
+	
+	public Map(LinkedList<Building> buildingList, String buildingName, int height, int width) {
+		super();
+		this.buildingList = buildingList;
+		this.buildingName = buildingName;
+		this.height = height;
+		this.width = width;
+	}
+
+
+	public LinkedList<Building> getBuildingList() {
+		return buildingList;
+	}
+
+
+	public void setBuildingList(LinkedList<Building> buildingList) {
+		this.buildingList = buildingList;
+	}
+
+
+	public String getBuildingName() {
+		return buildingName;
+	}
+
+
+	public void setBuildingName(String buildingName) {
+		this.buildingName = buildingName;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void addBuilding(Building b) {
+		this.buildingList.add(b);
+	}
+	
+	public void removeBuilding(Building b) {
+		this.buildingList.remove(b);
+	}
+	
 }
