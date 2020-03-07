0241d3d1a23228406c7a5c93a9282386e4f40578
Michael Alvarado <michael.alvarado1@upr.edu>
2020-03-07 13:07:41 -0400

    added print to console time it takes to load the app (will be use for efficiency)

--- src/main/Launch.java
+++ src/main/Launch.java
@@ -13,8 +13,11 @@ public class Launch {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					System.out.println("Loading App...");
+					long start = System.nanoTime();
 					Display frame = new Display("Project",1000,800);
 					MapDesign designMap = new MapDesign (frame);
+					System.out.println("App Loaded in: " + ((System.nanoTime()-start)/1000000000.0) + " seconds");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
