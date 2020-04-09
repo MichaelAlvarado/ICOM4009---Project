c333d02c465bea6fa96ec570729d69ba581659c3
Jorge Calderon <jorge.calderon3@upr.edu>
2020-04-09 16:58:53 -0400

    Config file completely reworked. Added questions file.

--- src/Components/Map.java
+++ src/Components/Map.java
@@ -194,7 +194,6 @@ public class Map {
 	public void generateTextFile() {
 
 		WriteFile data = new WriteFile(mapName +"ConfigurationFile.txt", false);
-		int counter = 1;
 
 		//Write the map information first, no iteration needed for this.
 		String file = "Map: " + this.getMapName() + "\n"
@@ -202,26 +201,39 @@ public class Map {
 				+ "Image: " + this.getImageURL() + "\n";
 
 		//Iterate through list of buildings to store info on file
-		for(Building buildings: buildingList){
-			file += "\nBuilding #" + counter + ":\n" 
-					+ "Name: " + buildings.getName() + "\n" 
-					+ "Building Image: " + buildings.getPictureURL() + "\n"
-					+ buildings.getWallInfo() //this method add printLine already
-					+ buildings.getQuestions() 
-					+ "Found: " + buildings.getFound() + "\n";
-			counter++;
+		for(Building b: buildingList){
+			file += "\nBuilding Name: " + b.getName() + "\n" 
+					+ "Building Image: " + b.getPictureURL() + "\n"
+					+ b.getWallInfo(); //this method add printLine already
 		}
 		file += "\nTrees:";
 		//Iterate through all the trees in the map
 		for(Tree trees: trees) {
 			file += trees.getTreeInfo();
 		}
+		
+		file += "\n\nQuestionsFile: " + mapName + "TriviaFile.txt";
 		//Write String file to the Configuration File
 		try {
 			data.writeToFile(file);
 		} catch (IOException e) {
 			System.out.println("Something went wrong!");
 		}	
+	}
+	
+	public void generateQuestionFile() {
+		WriteFile data = new WriteFile(mapName + "TriviaFile.txt", false);
+		String file = "";
+		for (Building b: buildingList) {
+			file += "Building: " + b.getName() + "\n"
+					+ b.getQuestions();
+		}
+		
+		try {
+			data.writeToFile(file);
+		} catch (IOException e) {
+			System.out.println("Something went wrong!");
+		}
 	}
 
 	/**
@@ -235,9 +247,9 @@ public class Map {
 	 */
 	public void generateMap(File file) throws FileNotFoundException {
 		Scanner sc = new Scanner(file);
-		String wallLine;
+		String filepath = "";
+		String wallLine = "";
 		String questionLine;
-		String foundLine = "";
 		String treeLine;
 		String breakLine = "";
 
@@ -259,10 +271,10 @@ public class Map {
 		//Load Buildings
 		while (sc.hasNextLine()) {
 			//create a building
-			if(breakLine.contains("Building #")) {
+			while(breakLine.contains("Building Name")) {
 				//name of Building
-				String name = sc.nextLine();
-				name = name.substring(name.indexOf("Name:")+6); //+6 to exclude "Name: "
+				String name = breakLine.substring(breakLine.indexOf("Name:")+6); //+6 to exclude "Name: "
+				//System.out.println(name);
 				building = new Building(name);
 				//Building Image 
 				String bImage = sc.nextLine(); 
@@ -284,130 +296,150 @@ public class Map {
 					String height = wallLine.substring(wallLine.indexOf("Height:")+8, index);
 					//PX1
 					index = wallLine.indexOf(',',index+1);
-					String pointX1 = wallLine.substring(wallLine.indexOf("First Point:")+13, index);
+					String pointX1 = wallLine.substring(wallLine.indexOf("First Point:")+14, index);
 					//PY1
-					String pointY1 = wallLine.substring(index+2, index = wallLine.indexOf(',',index+1));
+					String pointY1 = wallLine.substring(index+2, index = wallLine.indexOf(')',index+1));
 					//PX2
 					index = wallLine.indexOf(',',index+1);
-					String pointX2 = wallLine.substring(wallLine.indexOf("Second Point:")+14, index);
+					index = wallLine.indexOf(',',index+1);
+					String pointX2 = wallLine.substring(wallLine.indexOf("Second Point:")+15, index);
 					//PY2
-					String pointY2 = wallLine.substring(index+2, index = wallLine.indexOf(',',index+1));
+					String pointY2 = wallLine.substring(index+2, wallLine.indexOf(')', index));
 					//Image
+					wallLine=sc.nextLine();
 					String wImage = wallLine.substring(wallLine.indexOf("Wall Image:" ) + 12);
 
-					//					System.out.println(name);
-					//					System.out.println(height);
-					//					System.out.println(pointX1);
-					//					System.out.println(pointY1);
-					//					System.out.println(pointX2);
-					//					System.out.println(pointY2);
-					//					System.out.println(wImage);
+//										System.out.println(name);
+//									System.out.println(height);
+//										System.out.println(pointX1);
+//										System.out.println(pointY1);
+//										System.out.println(pointX2);
+//										System.out.println(pointY2);
+//										System.out.println(wImage);
 
-					Wall wall = new Wall(name, Integer.valueOf(height), 
+					Wall wall = new Wall(name, 
 							new Point(Integer.valueOf(pointX1), Integer.valueOf(pointY1)),
 							new Point(Integer.valueOf(pointX2), Integer.valueOf(pointY2)));
+					wall.setHeight(Integer.valueOf(height));
 					wall.setTextureURL(wImage);
 					building.addWalls(wall);
 					wallLine=sc.nextLine();
+					if (wallLine.isEmpty())
+						breakLine = sc.nextLine();
 
 				}
-				//Load Questions 
-				questionLine=wallLine;
-				String answ1, answ2, answ3, answ4;
-				while(questionLine.contains("Question:")){
-					int index = questionLine.indexOf("Correct Answer:") - 2;
-					//Get the question
-					name = questionLine.substring(questionLine.indexOf("Question:")+10,index);
-					index = questionLine.indexOf("Incorrect Answer1:")-2;
-					//First Answer (Correct Answer)
-					answ1 = questionLine.substring((questionLine.indexOf("Correct Answer:") + 16), index);
-					index = questionLine.indexOf("Incorrect Answer2:") - 2;
-					//Second Answer
-					answ2 = questionLine.substring((questionLine.indexOf("Incorrect Answer1:") + 19), index);
-					index = questionLine.indexOf("Incorrect Answer3:") - 2;
-					//Third Answer
-					answ3 = questionLine.substring((questionLine.indexOf("Incorrect Answer2:") + 19), index);
-					//Fourth Answer
-					answ4 = questionLine.substring(questionLine.indexOf("Incorrect Answer3: ") + 19);
-
-//										System.out.println(name);
-//										System.out.println(answ1);
-//										System.out.println(answ2);
-//										System.out.println(answ3);
-//										System.out.println(answ4);
-
-					Question question = new Question(name, answ1, answ2, answ3, answ4);
-					building.addQuestion(question);
-					questionLine=sc.nextLine();	
-				}
-
-				//Load Found 
-				foundLine = questionLine;
-				name = foundLine.substring(foundLine.indexOf("Found:") + 7);
-				boolean found = Boolean.valueOf(name);
-				building.setFound(found);
-				//System.out.println(name);
-				foundLine = sc.nextLine();
-				foundLine = breakLine;
-				foundLine = sc.nextLine();
 				
 				//Add Building
 				this.addBuilding(building);
 				//System.out.println("Load Done");
 			}
 			////// Load Trees
-			treeLine = foundLine;
+			treeLine = breakLine;
+			//System.out.println(treeLine);
 			if(treeLine.contains("Trees:")) {
 				if (sc.hasNext())
 						treeLine = sc.nextLine();
 				int index = 0;
-				while(sc.hasNextLine()) {
+				while(treeLine.contains("ID:")) {
 					Tree t = new Tree();
 					index = treeLine.indexOf(',');
-					if(treeLine.contains("ID:")) {
-						//ID of Tree
-						String treeID = treeLine.substring(treeLine.indexOf("ID:") + 4, index);
-						//System.out.println(treeID);
 					
-						index = treeLine.indexOf(',', index + 1);
-						//Species of Tree
-						String treeSpecies = treeLine.substring(treeLine.indexOf("Species:") + 9);
-						//System.out.println(treeSpecies);
-//						index = treeLine.indexOf(',', index + 1);
-//						//Height of Tree
-//						String treeHeight = treeLine.substring(treeLine.indexOf("Height:") + 8);
-//						System.out.println(treeHeight);
-						t.setID(treeID);
-						t.setTreeSpecies(Integer.valueOf(treeSpecies));
-//						t.setTreeHeight(Integer.valueOf(treeHeight));
-					}
+					//ID of Tree
+					String treeID = treeLine.substring(treeLine.indexOf("ID:") + 4, index);
+					//System.out.println(treeID);
 					
-					treeLine = sc.nextLine();
-					
-					if(treeLine.contains("Position:")) {
-						index = treeLine.indexOf(',');
-						//Coordinates of First Point
-						String p1X = treeLine.substring(treeLine.indexOf("Position:") + 10, index);
-						String p1Y = treeLine.substring(index + 2);
-						//System.out.println(p1X + ", " + p1Y);
-						t.setP1(new Point(Integer.valueOf(p1X), Integer.valueOf(p1Y))); 
-					}
-					
+					index = treeLine.indexOf(',', index + 1);
+					//Species of Tree
+					String treeSpecies = treeLine.substring(treeLine.indexOf("Species:") + 9, index);
+					//System.out.println(treeSpecies);
+
+					index = treeLine.indexOf(',', index + 1);
+					String p1X = treeLine.substring(treeLine.indexOf("Position:") + 11, index);
+					String p1Y = treeLine.substring(index + 2, index = treeLine.indexOf(')', index + 1));
+					t.setID(treeID);
+					t.setTreeSpecies(Integer.valueOf(treeSpecies));
+					t.setP1(new Point(Integer.valueOf(p1X), Integer.valueOf(p1Y))); 
+					//System.out.println(p1X + ", " + p1Y);
 					
 					//treeLine = sc.nextLine();
 					if(!(t.getID() == null)) {
 						trees.add(t);
 					}
-					
+					treeLine = sc.nextLine();
 				}
 			}
+			questionLine = sc.nextLine();
+			String q = questionLine.substring(questionLine.indexOf("QuestionsFile: ") + 15);
+			//System.out.println(q);
+			String OS = System.getProperty("os.name").toLowerCase();
+			if(OS.indexOf("mac") >= 0) {
+				 filepath = file.getParentFile().toString() + "/" + mapName + "TriviaFile.txt";
+				// System.out.println("aqui");
+			}
+			else {
+				 filepath = file.getParentFile().toString() + "\\" + mapName + "TriviaFile.txt";
+				 //System.out.println("aqui2");
+			}
+			//System.out.println(filepath);
+			Scanner sc2 = new Scanner (new File(filepath));
+			//System.out.println(sc2.nextLine());
+			String bname = sc2.nextLine();
+			String qname = "";
+			String rAns = "";
+			String w1Ans = "";
+			String w2Ans = "";
+			String w3Ans = "";
+			int counter = 0;
+			while(sc2.hasNextLine()) {
+				//System.out.println("llegamos");
+				if(bname.contains("Building:")) {
+					//System.out.println("building");
+
+					bname = bname.substring(bname.indexOf("Building:") + 10);
+					//System.out.println(bname);
+				}
+				
+				while (counter < 4) {
+					qname = sc2.nextLine();
+					qname = qname.substring(qname.indexOf("Question:") + 10);
+					//System.out.println(qname);
+					rAns = sc2.nextLine();
+					
+					
+					rAns = rAns.substring(rAns.indexOf("Right:") + 7);
+					//System.out.println(rAns);
+					w1Ans = sc2.nextLine();
+					
+					
+					w1Ans = w1Ans.substring(w1Ans.indexOf("Wrong:") + 7);
+					//System.out.println(w1Ans);
+					w2Ans = sc2.nextLine();
+					
+					
+					w2Ans = w2Ans.substring(w2Ans.indexOf("Wrong:") + 7);
+					//System.out.println(w2Ans);
+					w3Ans = sc2.nextLine();
+						
 			
+					w3Ans = w3Ans.substring(w3Ans.indexOf("Wrong:") + 7);
+					//System.out.println(w3Ans);
+					
+					Question question = new Question(qname, rAns, w1Ans, w2Ans, w3Ans);
+					
+					for (Building b: buildingList){
+						if (b.getName().equals(bname)) {
+							//System.out.println("for loop");
+							b.addQuestion(question);;
+						}
+					}
+					counter++;
+				}
+				counter = 0;
+				bname = sc2.nextLine();
+			
+			}
 		}
 
 	}
 
 }
-
-
-
-
